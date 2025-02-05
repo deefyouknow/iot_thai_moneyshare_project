cdca3d75b4834c665d7fc70a955aa38f80ee4578
deefyouknow <deefyouknow@gmail.com>
2025-02-05 10:14:56 +0700

    v 0.0.2

---
+++ lib/views/money_input_io.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MoneyInputIo extends StatefulWidget {
+  const MoneyInputIo({super.key});
+
+  @override
+  State<MoneyInputIo> createState() => _MoneyInputIoState();
+}
+
+class _MoneyInputIoState extends State<MoneyInputIo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
